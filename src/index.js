ffb70172390a0b0704a9b814f0a3c0b7f4d1eae9
Paulo Victor <paulovictor.devs@gmail.com>
2023-09-29 21:52:02 -0300

    a

--- src/index.js
+++ src/index.js
@@ -10,20 +10,13 @@ const content = document.getElementById('content');
 
 content.appendChild(taskTable()); // This starts the task table
 
-const task = new Task('Buy my P.O 33 K.O','Buy as soon as possible to make the beats!','12/4','high'); //this will work through event listenner;
+const task = new Task('Buy my P.O 33 K.O','Buy as soon as possible to make the beats!','12/4','high');
 const task2 = new Task('Learn DJ STUF!','Learn to make the beats!','2024-02-14','high'); //this will work through event listenner;
 
 tasks.push(task2); // delete later
 content.appendChild(fillTasksTable()); // delete later
 
-// Move this to separate module later
-/*
-document.body.addEventListener('click', e => {
-    editTask(e);
-});
-*/
-document.body.addEventListener('click', editTask);
-
+//document.body.addEventListener('click', editTask);
 
 //const test2 = createTask(task2);  comment this to avoid conflicts while build edit-tasks module
 //content.appendChild(test2);
